9eea73ac48f41d7bb98914ec1c9c8c9dc9fd61f9
Donate_ro <joke4789@ya.ru>
2019-02-14 19:39:15 +0300

    DataSource is replaced from the database file to the dynamical DataTable object

--- VB/FormMain.Designer.vb
+++ VB/FormMain.Designer.vb
@@ -18,95 +18,85 @@ Namespace SelfReferenced
 			MyBase.Dispose(disposing)
 		End Sub
 
-		#Region "Windows Form Designer generated code"
+#Region "Windows Form Designer generated code"
 
-		''' <summary>
-		''' Required method for Designer support - do not modify
-		''' the contents of this method with the code editor.
-		''' </summary>
-		Private Sub InitializeComponent()
-			Me.dataset11 = New SelfReferenced.Dataset1()
-			Me.gridControl1 = New DevExpress.XtraGrid.GridControl()
-			Me.gridView1 = New DevExpress.XtraGrid.Views.Grid.GridView()
-			Me.colDepartment = New DevExpress.XtraGrid.Columns.GridColumn()
-			Me.colBudget = New DevExpress.XtraGrid.Columns.GridColumn()
-			Me.colLocation = New DevExpress.XtraGrid.Columns.GridColumn()
-			CType(Me.dataset11, System.ComponentModel.ISupportInitialize).BeginInit()
-			CType(Me.gridControl1, System.ComponentModel.ISupportInitialize).BeginInit()
-			CType(Me.gridView1, System.ComponentModel.ISupportInitialize).BeginInit()
-			Me.SuspendLayout()
-			' 
-			' dataset11
-			' 
-			Me.dataset11.DataSetName = "Dataset1"
-			Me.dataset11.Locale = New System.Globalization.CultureInfo("en-US")
-			Me.dataset11.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema
-			' 
-			' gridControl1
-			' 
-			Me.gridControl1.Dock = System.Windows.Forms.DockStyle.Fill
-			Me.gridControl1.Location = New System.Drawing.Point(0, 0)
-			Me.gridControl1.MainView = Me.gridView1
-			Me.gridControl1.Name = "gridControl1"
-			Me.gridControl1.Size = New System.Drawing.Size(597, 366)
-			Me.gridControl1.TabIndex = 1
-			Me.gridControl1.ViewCollection.AddRange(New DevExpress.XtraGrid.Views.Base.BaseView() { Me.gridView1})
-			' 
-			' gridView1
-			' 
-			Me.gridView1.Columns.AddRange(New DevExpress.XtraGrid.Columns.GridColumn() { Me.colDepartment, Me.colBudget, Me.colLocation})
-			Me.gridView1.DetailHeight = 1000
-			Me.gridView1.GridControl = Me.gridControl1
-			Me.gridView1.Name = "gridView1"
-			Me.gridView1.OptionsView.ShowGroupPanel = False
-'			Me.gridView1.MasterRowGetLevelDefaultView += New DevExpress.XtraGrid.Views.Grid.MasterRowGetLevelDefaultViewEventHandler(Me.gridView1_MasterRowGetLevelDefaultView);
-			' 
-			' colDepartment
-			' 
-			Me.colDepartment.Caption = "Department"
-			Me.colDepartment.FieldName = "Department"
-			Me.colDepartment.Name = "colDepartment"
-			Me.colDepartment.Visible = True
-			Me.colDepartment.VisibleIndex = 0
-			' 
-			' colBudget
-			' 
-			Me.colBudget.Caption = "Budget"
-			Me.colBudget.DisplayFormat.FormatString = "c0"
-			Me.colBudget.DisplayFormat.FormatType = DevExpress.Utils.FormatType.Numeric
-			Me.colBudget.FieldName = "Budget"
-			Me.colBudget.Name = "colBudget"
-			Me.colBudget.Visible = True
-			Me.colBudget.VisibleIndex = 1
-			' 
-			' colLocation
-			' 
-			Me.colLocation.Caption = "Location"
-			Me.colLocation.FieldName = "Location"
-			Me.colLocation.Name = "colLocation"
-			Me.colLocation.Visible = True
-			Me.colLocation.VisibleIndex = 2
-			' 
-			' FormMain
-			' 
-			Me.AutoScaleDimensions = New System.Drawing.SizeF(6F, 13F)
-			Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-			Me.ClientSize = New System.Drawing.Size(597, 366)
-			Me.Controls.Add(Me.gridControl1)
-			Me.Name = "FormMain"
-			Me.Text = "FormMain"
-'			Me.Load += New System.EventHandler(Me.Form1_Load);
-			CType(Me.dataset11, System.ComponentModel.ISupportInitialize).EndInit()
-			CType(Me.gridControl1, System.ComponentModel.ISupportInitialize).EndInit()
-			CType(Me.gridView1, System.ComponentModel.ISupportInitialize).EndInit()
-			Me.ResumeLayout(False)
+        ''' <summary>
+        ''' Required method for Designer support - do not modify
+        ''' the contents of this method with the code editor.
+        ''' </summary>
+        Private Sub InitializeComponent()
+            Me.gridControl1 = New DevExpress.XtraGrid.GridControl()
+            Me.gridView1 = New DevExpress.XtraGrid.Views.Grid.GridView()
+            Me.colDepartment = New DevExpress.XtraGrid.Columns.GridColumn()
+            Me.colBudget = New DevExpress.XtraGrid.Columns.GridColumn()
+            Me.colLocation = New DevExpress.XtraGrid.Columns.GridColumn()
+            CType(Me.gridControl1, System.ComponentModel.ISupportInitialize).BeginInit()
+            CType(Me.gridView1, System.ComponentModel.ISupportInitialize).BeginInit()
+            Me.SuspendLayout()
+            ' 
+            ' gridControl1
+            ' 
+            Me.gridControl1.Dock = System.Windows.Forms.DockStyle.Fill
+            Me.gridControl1.Location = New System.Drawing.Point(0, 0)
+            Me.gridControl1.MainView = Me.gridView1
+            Me.gridControl1.Name = "gridControl1"
+            Me.gridControl1.Size = New System.Drawing.Size(597, 366)
+            Me.gridControl1.TabIndex = 1
+            Me.gridControl1.ViewCollection.AddRange(New DevExpress.XtraGrid.Views.Base.BaseView() {Me.gridView1})
+            ' 
+            ' gridView1
+            ' 
+            Me.gridView1.Columns.AddRange(New DevExpress.XtraGrid.Columns.GridColumn() {Me.colDepartment, Me.colBudget, Me.colLocation})
+            Me.gridView1.DetailHeight = 1000
+            Me.gridView1.GridControl = Me.gridControl1
+            Me.gridView1.Name = "gridView1"
+            Me.gridView1.OptionsView.ShowGroupPanel = False
+            AddHandler Me.gridView1.MasterRowGetLevelDefaultView, AddressOf Me.gridView1_MasterRowGetLevelDefaultView
+            ' 
+            ' colDepartment
+            ' 
+            Me.colDepartment.Caption = "Department"
+            Me.colDepartment.FieldName = "Department"
+            Me.colDepartment.Name = "colDepartment"
+            Me.colDepartment.Visible = True
+            Me.colDepartment.VisibleIndex = 0
+            ' 
+            ' colBudget
+            ' 
+            Me.colBudget.Caption = "Budget"
+            Me.colBudget.DisplayFormat.FormatString = "c0"
+            Me.colBudget.DisplayFormat.FormatType = DevExpress.Utils.FormatType.Numeric
+            Me.colBudget.FieldName = "Budget"
+            Me.colBudget.Name = "colBudget"
+            Me.colBudget.Visible = True
+            Me.colBudget.VisibleIndex = 1
+            ' 
+            ' colLocation
+            ' 
+            Me.colLocation.Caption = "Location"
+            Me.colLocation.FieldName = "Location"
+            Me.colLocation.Name = "colLocation"
+            Me.colLocation.Visible = True
+            Me.colLocation.VisibleIndex = 2
+            ' 
+            ' FormMain
+            ' 
+            Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0F, 13.0F)
+            Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+            Me.ClientSize = New System.Drawing.Size(597, 366)
+            Me.Controls.Add(Me.gridControl1)
+            Me.Name = "FormMain"
+            Me.Text = "FormMain"
+            AddHandler Me.Load, AddressOf Me.Form1_Load
+            CType(Me.gridControl1, System.ComponentModel.ISupportInitialize).EndInit()
+            CType(Me.gridView1, System.ComponentModel.ISupportInitialize).EndInit()
+            Me.ResumeLayout(False)
 
-		End Sub
+        End Sub
 
-		#End Region
+#End Region
 
-		Private dataset11 As Dataset1
-		Private gridControl1 As DevExpress.XtraGrid.GridControl
+        Private gridControl1 As DevExpress.XtraGrid.GridControl
 		Private WithEvents gridView1 As DevExpress.XtraGrid.Views.Grid.GridView
 		Private colDepartment As DevExpress.XtraGrid.Columns.GridColumn
 		Private colBudget As DevExpress.XtraGrid.Columns.GridColumn
--- VB/FormMain.vb
+++ VB/FormMain.vb
@@ -1,38 +1,57 @@
-Imports Microsoft.VisualBasic
-Imports System
-Imports System.Collections.Generic
-Imports System.ComponentModel
-Imports System.Data
-Imports System.Drawing
-Imports System.Text
-Imports System.Windows.Forms
 Imports DevExpress.XtraGrid.Views.Grid
+Imports System
+Imports System.Data
+Imports System.Windows.Forms
 
 Namespace SelfReferenced
-	Partial Public Class FormMain
-		Inherits Form
-		<STAThread> _
-		Shared Sub Main()
-			Application.Run(New FormMain())
-		End Sub
+    Partial Public Class FormMain
+        Inherits Form
 
-		Public Sub New()
-			InitializeComponent()
-		End Sub
+        Private dataSet11 As DataSet
 
-		Private Sub Form1_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles MyBase.Load
-			dataset11.ReadXml("data.xml")
-			Dim dv As New DataView(dataset11.Products)
-			dv.RowFilter = "([ParentID] Is NULL)"
-			gridControl1.DataSource = dv
-			gridView1.OptionsDetail.ShowDetailTabs = False
-		End Sub
+        Public Sub New()
+            InitializeComponent()
+        End Sub
 
-		Private Sub gridView1_MasterRowGetLevelDefaultView(ByVal sender As Object, ByVal e As DevExpress.XtraGrid.Views.Grid.MasterRowGetLevelDefaultViewEventArgs) Handles gridView1.MasterRowGetLevelDefaultView
-			Dim view As New GridView(gridControl1)
-			view.Assign(gridControl1.MainView, True)
-			view.OptionsView.ShowGroupPanel = False
-			e.DefaultView = view
-		End Sub
-	End Class
+        Private Sub Form1_Load(ByVal sender As Object, ByVal e As System.EventArgs)
+            dataSet11 = New DataSet()
+            dataSet11.Tables.Add(GetProductsDataTable())
+            dataSet11.Relations.Add("ProductsProducts", dataSet11.Tables("Products").Columns("ID"), dataSet11.Tables("Products").Columns("ParentID"))
+            Dim dv As DataView = New DataView(dataSet11.Tables(0))
+            dv.RowFilter = "([ParentID] Is NULL)"
+            gridControl1.DataSource = dv
+            gridView1.OptionsDetail.ShowDetailTabs = False
+        End Sub
+
+        Private Function GetProductsDataTable() As DataTable
+            Dim table As DataTable = New DataTable()
+            table.TableName = "Products"
+            table.Columns.Add(New DataColumn("ID", GetType(Int32)))
+            table.Columns.Add(New DataColumn("ParentID", GetType(Int32)))
+            table.Columns.Add(New DataColumn("ImageIndex", GetType(Integer)))
+            table.Columns.Add(New DataColumn("Department", GetType(String)))
+            table.Columns.Add(New DataColumn("Budget", GetType(Integer)))
+            table.Columns.Add(New DataColumn("Location", GetType(String)))
+            table.Rows.Add(0, Nothing, 0, "Departament " & 0, 1000, "Location " & 0)
+
+            For i As Integer = 0 To 20 - 1
+                Dim index As Integer = i + 1
+                table.Rows.Add(index, i Mod 4, i Mod 5, "Departament " & index, (index Mod 7) * 1000, "Location " & index)
+            Next
+
+            Return table
+        End Function
+
+        Private Sub gridView1_MasterRowGetLevelDefaultView(ByVal sender As Object, ByVal e As DevExpress.XtraGrid.Views.Grid.MasterRowGetLevelDefaultViewEventArgs)
+            Dim view As GridView = New GridView(gridControl1)
+            view.Assign(gridControl1.MainView, True)
+            view.OptionsView.ShowGroupPanel = False
+            e.DefaultView = view
+        End Sub
+
+        <STAThread>
+        Public Shared Sub Main()
+            Application.Run(New FormMain())
+        End Sub
+    End Class
 End Namespace
